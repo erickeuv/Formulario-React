01cd60d6555cff369b5faad9bf00c8203e8f5cdd
Erick Urdaneta <133135692+erickeuv@users.noreply.github.com>
2024-09-27 20:29:17 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://erickeuv.github.io/Formulario-React/'
+  base: 'erickeuv.github.io/Formulario-React/'
 })
